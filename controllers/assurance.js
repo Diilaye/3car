2044911b7153a5f6f304afae7748dcf28461bff1
Issa laye KANE <issa.k@deally.fr>
2024-05-01 16:32:38 +0000

    add news garantis

--- controllers/assurance.js
+++ controllers/assurance.js
@@ -1,10 +1,5 @@
 const assuranceModel = require("../models/assurance");
 
-const garantiModel = require('../models/garanti.js');
-
-const souscripteurModel = require('../models/souscripteur.js');
-
-const vehiculeModel = require('../models/vehicule.js');
 
 
 exports.add = async (req, res, next) => {
@@ -14,94 +9,24 @@ exports.add = async (req, res, next) => {
     try {
 
         let {
-            nom_assure,
-            adresse_assure,
-            tel_assure,
-            genre,
-            marque,
-            immat,
-            place,
-            puissance,
-            police,
-            attestation,
-            effet,
-            durer,
-            echeance,
-            temoin,
-            cause
+            code,
+            phone
         } = req.query;
 
-        let vehicule = {};
+        const assurance = assuranceModel();
 
-        const garanti = garantiModel();
+        assurance.code = code;
+        assurance.phone = phone;
 
+        const saveAssurance = await assurance.save();
 
-        vehicule = await vehiculeModel.findOne({
-            marque: marque
-        }).exec();
-
-        console.log("vehicule");
-        console.log(vehicule);
-
-        if (!vehicule) {
-            vehicule = vehiculeModel();
-
-            vehicule.genre = genre;
-            vehicule.marque = marque;
-            vehicule.imatriculation = immat;
-            vehicule.place = place;
-            vehicule.puissance_fiscale = puissance;
-            const vehiculeSave = await vehicule.save();
-            garanti.vehiculeGaranti = vehiculeSave.id;
-
-        } else {
-            garanti.vehiculeGaranti = vehicule.id;
-        }
-
-        const souscripteur = souscripteurModel();
-
-        souscripteur.adresse = adresse_assure;
-        souscripteur.assure = nom_assure;
-        souscripteur.telephone = tel_assure;
-        const souscripteurSave = await souscripteur.save();
-
-
-        garanti.police = police;
-        garanti.temoin = temoin;
-        garanti.attestation = attestation;
-        garanti.effet = effet;
-        garanti.echeance = echeance;
-        garanti.durer = durer;
-        garanti.cause = cause;
-        garanti.soucripteurGaranti = souscripteurSave.id;
-
-        const garantiSave = await garanti.save();
 
         return res.status(201).json({
-            message: 'creation reussi',
-            data: garantiSave,
-        })
-
-
-        // let {
-        //     code,
-        //     phone
-        // } = req.query;
-
-        // const assurance = assuranceModel();
-
-        // assurance.code = code;
-        // assurance.phone = phone;
-
-        // const saveAssurance = await assurance.save();
-
-
-        // return res.status(201).json({
-        //     message: 'creation réussi',
-        //     status: 'OK',
-        //     data: saveAssurance,
-        //     statusCode: 201
-        // });
+            message: 'creation réussi',
+            status: 'OK',
+            data: saveAssurance,
+            statusCode: 201
+        });
 
     } catch (error) {
         return res.status(404).json({
